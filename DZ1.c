edc06a44d5d2fb840f0febdcebea5c7a33725227
Anya442 <91282788+Anya442@users.noreply.github.com>
2021-12-02 22:02:51 +0300

    Create DZ1.c

---
+++ DZ1.c
@@ -0,0 +1,74 @@
+# include <stdio.h> 
+# include <stdlib.h>
+# include <windows.h>
+# include <ShellApi.h> 
+#define swap(x,y) {int t = a; a = b; b = t;}
+int main(void)
+{
+
+	int rebro_count = 0;
+	int node_count = 0;
+	int a = 0, b = 0;
+	int link[255];
+	for (int i = 0; i < 255; i++)
+		link[i] = -1;
+
+	FILE* S1;
+	fopen_s(&S1, "S1.dot", "w");
+	fprintf(S1, "%s\n", "graph My_graph {");
+
+	while (scanf_s("%d-%d", &a, &b) != 0)
+	{
+		fprintf(S1, "%d -- %d;\n", a, b);
+		rebro_count++;
+
+		if (link[a] * link[b] < 0)
+		{
+			if (a < b)
+				swap(a, b);
+			link[a] = link[b];
+			node_count++;
+		}
+		else if ((link[a] * link[b] == 1) && (link[a] != 1))
+		{
+			link[a] = link[b] = rebro_count;
+			node_count += 2;
+		}
+		else
+		{
+			if (a < b)
+				swap(a, b);
+			int find = link[a];
+			for (int i = 0; i < 255; i++)
+			{
+				if (link[i] == find)
+				{
+					link[i] = link[b];
+				}
+			}
+		}
+	}
+
+	fprintf(S1, "%s", "}");
+	fclose(S1);
+
+	int is_tree = 1;
+	for (int i = 1; i < 255; i++)
+	{
+		if ((link[i] != -1) && (link[i] != 1))
+			is_tree = 0;
+	}
+
+	if ((node_count - (rebro_count)) != 1)
+		is_tree = 0;
+
+	if (is_tree)
+		printf("%s", "The graph is tree");
+	else
+		printf("%s", "The graph is not tree");
+
+	system("dot S1.dot -Tbmp -o S1.bmp");
+	ShellExecuteA(GetDesktopWindow(), "open", "C:/Users/Аня/Documents/dz//Project1/S1.bmp", NULL, NULL, SW_SHOW);
+
+	return 0;
+}
